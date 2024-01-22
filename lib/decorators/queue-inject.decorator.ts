f2e13632cfe5558500a0b3e8cd7fd62ce4a59fb5
he2lium <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>
2024-01-22 10:50:45 +0700

    many queues

--- lib/decorators/queue-inject.decorator.ts
+++ lib/decorators/queue-inject.decorator.ts
@@ -1,3 +1,4 @@
 import {Inject} from "@nestjs/common";
+import {getClientTokenUtil} from "../utils/get-client-token.util";
 
-export const InjectQueue = (token: string) => Inject(`YCMQ_FEATURE_${token}_CLIENT`)
+export const InjectQueue = (token: string) => Inject(getClientTokenUtil(token))
